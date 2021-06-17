3963110aafb0e4fb49631087217bd3f820cbf657
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-06-17 00:35:54 -0300

    Reconfigurado API para usar Sequelize e criado banco de dados com o mesmo

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'mysql',
+  username: 'root',
+  password: '#Tsunami21',
+  database: 'sqlnode',
+  host: 'localhost',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+}
